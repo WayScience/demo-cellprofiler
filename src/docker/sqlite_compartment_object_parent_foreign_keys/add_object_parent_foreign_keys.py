2af573269cb56c377b768fec149b28cde2a40623
d33bs <dave.bunten@cuanschutz.edu>
2024-09-30 14:57:22 -0600

    updates with modified tables

--- src/docker/sqlite_compartment_object_parent_foreign_keys/add_object_parent_foreign_keys.py
+++
@@ -1,6 +0,0 @@
-"""
-Used to help add foreign keys for object parent columns
-within CellProfiler SQLite columns.
-"""
-
-
--- src/docker/sqlite_compartment_object_parent_foreign_keys/run.sh
+++ src/docker/sqlite_compartment_object_parent_foreign_keys/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # build and run cellprofiler from a docker container
-CPDOCKER_RUNDIR=$PWD/src/docker/sqlite_foreign_keys
+CPDOCKER_RUNDIR=$PWD/src/docker/sqlite_compartment_object_parent_foreign_keys
 CPDOCKER_IMAGE_NAME=cp-sqlite-fk
 
 # build image
@@ -26,3 +26,26 @@ docker run --rm --platform linux/amd64 -w /app \
     -v "$CPDOCKER_RUNDIR:/app" \
     "$CPDOCKER_IMAGE_NAME" \
     cellprofiler -c -r -p ExampleHuman_exporttodatabase.cppipe -o output -i ExampleHuman/images
+
+# set source and target databases
+SOURCE_DB="$CPDOCKER_RUNDIR/output/ExampleHuman.sqlite"
+TARGET_DB="$CPDOCKER_RUNDIR/ExampleHuman_modified.sqlite"
+
+# remove the targetdb to get a clean database
+rm -f $TARGET_DB
+
+# perform copies from the source to target databases using new schema
+sqlite3 $TARGET_DB <<EOF
+ATTACH DATABASE '$SOURCE_DB' AS source;
+.read $CPDOCKER_RUNDIR/examplehuman_modified.sql
+INSERT INTO Per_Image SELECT * FROM source.Per_Image;
+INSERT INTO Per_Cells SELECT * FROM source.Per_Cells;
+INSERT INTO Per_Nuclei SELECT * FROM source.Per_Nuclei;
+INSERT INTO Per_Cytoplasm SELECT * FROM source.Per_Cytoplasm;
+INSERT INTO Per_PH3 SELECT * FROM source.Per_PH3;
+INSERT INTO Experiment_Properties SELECT * FROM source.Experiment_Properties;
+INSERT INTO Per_Experiment SELECT * FROM source.Per_Experiment;
+INSERT INTO Per_RelationshipTypes SELECT * FROM source.Per_RelationshipTypes;
+INSERT INTO Per_Relationships SELECT * FROM source.Per_Relationships;
+VACUUM;
+EOF
